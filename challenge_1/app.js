25ae76c355d093feda263d5651ed300e5af7fe06
B.Maher <bouzid.meher@gmail.com>
2019-12-27 12:11:08 +0100

    click event handeled and dynamic insertion of the X and O done

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,19 +1,24 @@
 const cases = document.querySelectorAll(".playable")
-
-
+let X = document.createElement("IMG");
+X.src = "/Images/X.png";
 class Game {
     constructor() {
-        this.board = [
+        this._board = [
             [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]
         ];
+        //to keep track of the player : false for the first player and true for the second
+        this.currentPlayer = false;
+        //to see if the game started or not
+        this.game = false;
     };
+
     //calculate the score of the row
     checkRow(index) {
         let score = 0;
         for (var i = 0; i < 3; i++) {
-            score += board[index][i];
+            score += this._board[index][i];
         }
         return score;
     };
@@ -22,7 +27,7 @@ class Game {
     checkColumn(index) {
         let score = 0;
         for (var i = 0; i < 3; i++) {
-            score += board[i][index];
+            score += this._board[i][index];
         }
         return score;
     };
@@ -30,13 +35,37 @@ class Game {
     //calculate the score of the diagonal
     checkDaiagonal(rowIndex, columnIndex) {
         let score = 0;
-
         while (rowIndex < 3) {
-            score += board[rowIndex][columnIndex];
+            score += this._board[rowIndex][columnIndex];
             rowIndex++;
             columnIndex++;
         }
         return score;
+    };
+
+    //place the piece
+    place(rowIndex, columnIndex) {
+        this.game = true;
+        if (!this.currentPlayer) {
+            this._board[rowIndex][columnIndex] = 1;
+        } else {
+            this._board[rowIndex][columnIndex] = 2;
+        }
+        this.currentPlayer = !this.currentPlayer;
+    };
+
+    //check for the winner
+    checkWinner(rowIndex, columnIndex) {
+
+    }
+    //restart the game
+    restart() {
+        this._board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+        this.currentPlayer = false;
     };
 }
 
@@ -44,6 +73,23 @@ class Game {
 
 //need to hundle the click event
 //need to use forEach
-cases.addEventListener("click", (e) => {
-    console.log(e.target.id)
+let play = new Game()
+cases.forEach(elm => {
+    elm.addEventListener("click", (e) => {
+        // if(play.game){
+        // }
+        // appendChild(img)
+        let img = document.createElement("IMG");
+        if (!play.currentPlayer) {
+            img.src = "/Images/X.png";
+        } else {
+            img.src = "/Images/O.png";
+        }
+        e.target.appendChild(img)
+
+        let rowIndex = parseInt(e.target.id[0])
+        let columnIndex = parseInt(e.target.id[1])
+        play.place(rowIndex, columnIndex)
+        console.table(play._board)
+    })
 })
